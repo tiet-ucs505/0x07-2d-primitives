c6f47d355ddb9bc488a0f32a30726b8880183f94
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2024-03-21 05:51:37 +0530

    Canvas aspect ratio (with minimal sizes) to be set at source.

--- canvas-helpers.js
+++ canvas-helpers.js
@@ -4,10 +4,6 @@ const getDom = document.querySelector.bind(document)
 function canvasReset(sel) {
   const canvas = getDom(sel)
 
-  // Let the page reset as per Flex's and Floats
-  canvas.width = 5
-  canvas.height = 1
-
   // Get Dom Sizes
   const {bb:{width: W, height: H}} = getCanvas(sel)
 
